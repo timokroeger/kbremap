c0f711d6d79367726b04b5b38824d60872688265
Timo Kröger <timokroeger93@gmail.com>
2020-11-24 12:54:39 +0100

    Move rapping code to keyboard_hook module

--- src/keyboard_hook.rs
+++ src/keyboard_hook.rs
@@ -6,7 +6,7 @@ use winapi::{
     um::winuser::*,
 };
 
-thread_local!(static HOOK: RefCell<Option<Box<dyn FnMut(&KeyboardEvent) -> bool>>> = RefCell::new(None));
+thread_local!(static HOOK: RefCell<Option<Box<dyn FnMut(&KeyboardEvent) -> Remap>>> = RefCell::new(None));
 
 pub struct KeyboardHook<'a> {
     handle: HHOOK,
@@ -16,17 +16,18 @@ pub struct KeyboardHook<'a> {
 impl<'a> KeyboardHook<'a> {
     /// Sets the low-level keyboard hook for this thread.
     ///
-    /// If the closure returns `false`, the key event will not be forwarded to other
-    /// appliations.
+    /// Remaps the key event to a UTF-16 character if the closure returns `Some`.
+    /// Sends the character with a single virtual key if it is available on the current layout.
+    /// Uses `VK_PACKET` to pass Unicode characters as if they were keystrokes for everything else.
     ///
     /// Returns `None` when called more than once from the same thread.
-    pub fn set(callback: impl FnMut(&KeyboardEvent) -> bool + 'a) -> Option<KeyboardHook<'a>> {
+    pub fn set(callback: impl FnMut(&KeyboardEvent) -> Remap + 'a) -> Option<KeyboardHook<'a>> {
         HOOK.with(|hook| {
             if hook.borrow().is_some() {
                 return None;
             }
 
-            let boxed_cb: Box<dyn FnMut(&KeyboardEvent) -> bool + 'a> = Box::new(callback);
+            let boxed_cb: Box<dyn FnMut(&KeyboardEvent) -> Remap + 'a> = Box::new(callback);
             *hook.borrow_mut() = Some(unsafe { mem::transmute(boxed_cb) });
 
             Some(KeyboardHook {
@@ -88,6 +89,12 @@ impl KeyboardEvent {
     }
 }
 
+pub enum Remap {
+    Transparent,
+    Ignore,
+    Character(u16),
+}
+
 unsafe extern "system" fn hook_proc(code: c_int, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
     if code != 0 {
         return -1;
@@ -102,12 +109,66 @@ unsafe extern "system" fn hook_proc(code: c_int, w_param: WPARAM, l_param: LPARA
     }
 
     HOOK.with(|hook| {
-        let call_next_hook = hook.borrow_mut().as_mut().unwrap()(kb_event);
-        if call_next_hook {
-            CallNextHookEx(ptr::null_mut(), code, w_param, l_param)
-        } else {
-            // Swallow the key event
-            -1
+        let remap = hook.borrow_mut().as_mut().unwrap()(kb_event);
+        match remap {
+            Remap::Transparent => CallNextHookEx(ptr::null_mut(), code, w_param, l_param),
+            Remap::Ignore => -1,
+            Remap::Character(c) => {
+                send_char(kb_event, c);
+                -1
+            }
         }
     })
+}
+
+fn send_unicode(kb_event: &KeyboardEvent, c: u16) {
+    unsafe {
+        let mut kb_input: KEYBDINPUT = mem::zeroed();
+        kb_input.wScan = c;
+        kb_input.dwFlags = KEYEVENTF_UNICODE;
+        if kb_event.up() {
+            kb_input.dwFlags |= KEYEVENTF_KEYUP;
+        }
+        kb_input.time = kb_event.time();
+
+        let mut input: INPUT = mem::zeroed();
+        input.type_ = INPUT_KEYBOARD;
+        *input.u.ki_mut() = kb_input;
+
+        SendInput(1, &mut input, mem::size_of_val(&input) as _);
+    }
+}
+
+fn send_key(kb_event: &KeyboardEvent, vk: u8) {
+    unsafe {
+        let mut kb_input: KEYBDINPUT = mem::zeroed();
+        kb_input.wVk = vk as u16;
+        kb_input.wScan = kb_event.scan_code();
+        if kb_event.up() {
+            kb_input.dwFlags |= KEYEVENTF_KEYUP;
+        }
+        kb_input.time = kb_event.time();
+
+        let mut input: INPUT = mem::zeroed();
+        input.type_ = INPUT_KEYBOARD;
+        *input.u.ki_mut() = kb_input;
+
+        SendInput(1, &mut input, mem::size_of_val(&input) as _);
+    }
+}
+
+fn send_char(kb_event: &KeyboardEvent, c: u16) {
+    unsafe {
+        // TODO: Improve layout handling
+        let vk_state = VkKeyScanExW(c, GetKeyboardLayout(0));
+
+        // Send the character as unicode input if:
+        // 1. There is no key for the character available on the current keyboard layout
+        // 2. A modifier (bits the upper byte) is required to type this character
+        if vk_state == -1 || vk_state & 0xF00 != 0 {
+            send_unicode(kb_event, c);
+        } else {
+            send_key(kb_event, vk_state as u8);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,66 +2,16 @@
 
 mod keyboard_hook;
 
-use std::{collections::HashMap, ffi::OsStr, mem, os::windows::ffi::OsStrExt};
+use std::{collections::HashMap, ffi::OsStr, os::windows::ffi::OsStrExt};
 
-use keyboard_hook::{KeyboardEvent, KeyboardHook};
+use keyboard_hook::{KeyboardHook, Remap};
 
 use trayicon::{Icon, MenuBuilder, TrayIconBuilder};
-use winapi::um::{consoleapi::*, winuser::*};
+use winapi::um::{consoleapi::*};
 use winit::{
     event::Event,
     event_loop::{ControlFlow, EventLoop},
 };
-
-fn send_unicode(kb_event: &KeyboardEvent, c: u16) {
-    unsafe {
-        let mut kb_input: KEYBDINPUT = mem::zeroed();
-        kb_input.wScan = c;
-        kb_input.dwFlags = KEYEVENTF_UNICODE;
-        if kb_event.up() {
-            kb_input.dwFlags |= KEYEVENTF_KEYUP;
-        }
-
-        let mut input: INPUT = mem::zeroed();
-        input.type_ = INPUT_KEYBOARD;
-        *input.u.ki_mut() = kb_input;
-
-        SendInput(1, &mut input, mem::size_of_val(&input) as _);
-    }
-}
-
-fn send_key(kb_event: &KeyboardEvent, vk: u8) {
-    unsafe {
-        let mut kb_input: KEYBDINPUT = mem::zeroed();
-        kb_input.wVk = vk as u16;
-        kb_input.wScan = kb_event.scan_code();
-        if kb_event.up() {
-            kb_input.dwFlags |= KEYEVENTF_KEYUP;
-        }
-
-        let mut input: INPUT = mem::zeroed();
-        input.type_ = INPUT_KEYBOARD;
-        *input.u.ki_mut() = kb_input;
-
-        SendInput(1, &mut input, mem::size_of_val(&input) as _);
-    }
-}
-
-fn send_char(kb_event: &KeyboardEvent, c: u16) {
-    unsafe {
-        // TODO: Improve layout handling
-        let vk_state = VkKeyScanExW(c, GetKeyboardLayout(0));
-
-        // Send the character as unicode input if:
-        // 1. There is no key for the character available on the current keyboard layout
-        // 2. A modifier (bits the upper byte) is required to type this character
-        if vk_state == -1 || vk_state & 0xF00 != 0 {
-            send_unicode(kb_event, c);
-        } else {
-            send_key(kb_event, vk_state as u8);
-        }
-    }
-}
 
 fn main() {
     #[derive(Debug, Copy, Clone, Eq, PartialEq)]
@@ -121,13 +71,21 @@ fn main() {
     let mut active_layers = Vec::new();
 
     let _kbhook = KeyboardHook::set(|kb_event| {
+        println!(
+            "{} scan code: {}{:#06X}, virtual key: {:#04X}",
+            if kb_event.up() { '↑' } else { '↓' },
+            if kb_event.is_extended() { 'e' } else { ' ' },
+            kb_event.scan_code(),
+            kb_event.virtual_key(),
+        );
+
         if bypass {
-            return true;
+            return Remap::Transparent;
         }
 
         // TODO: Allow to remap extended scan codes.
         if kb_event.is_extended() {
-            return true;
+            return Remap::Transparent;
         }
 
         // Handle layer activation
@@ -144,7 +102,7 @@ fn main() {
                     .rposition(|&l| l == layer)
                     .map(|pos| active_layers.remove(pos));
             }
-            return false;
+            return Remap::Ignore;
         }
 
         let remapped_char = active_layers
@@ -154,11 +112,8 @@ fn main() {
             .get(&kb_event.scan_code());
 
         match remapped_char {
-            Some(&c) => {
-                send_char(kb_event, c);
-                false
-            }
-            None => true,
+            Some(&c) => Remap::Character(c),
+            None => Remap::Transparent,
         }
     });
 
