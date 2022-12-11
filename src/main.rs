2c91e9e346b861857c6d77534c92509a92389cb5
Timo Kröger <timokroeger93@gmail.com>
2022-12-11 19:45:48 +0100

    Refactor hook code
    Includes my first `let .. else ...`

--- src/main.rs
+++ src/main.rs
@@ -89,7 +89,8 @@ fn main() -> anyhow::Result<()> {
             kb.press_key(key_event.scan_code)
         };
 
-        // Special caps lock handling
+        // Special caps lock handling:
+        // Make sure the caps lock state stays in sync with the configured layer.
         if let Some(caps_lock_layer) = &config.caps_lock_layer {
             if (kb.locked_layer() == caps_lock_layer) != keyboard_hook::caps_lock_enabled() {
                 println!("toggle caps lock");
@@ -109,36 +110,31 @@ fn main() -> anyhow::Result<()> {
         ui.set_active_layer(kb.active_layer());
         ui.set_locked_layer(kb.locked_layer());
 
-        let mut log_line = key_event.to_string();
-
-        let handled = if let Some(remap) = remap {
-            match remap {
-                KeyAction::Ignore => {
-                    log_line.push_str(" ignored");
-                }
-                KeyAction::Character(c) => {
-                    if let Some(virtual_key) = keyboard_hook::get_virtual_key(c) {
-                        log_line = format!("{} remapped to `{}` as virtual key", log_line, c);
-                        key_event.key = KeyType::VirtualKey(virtual_key);
-                    } else {
-                        log_line = format!("{} remapped to `{}` as unicode input", log_line, c);
-                        key_event.key = KeyType::Unicode(c);
-                    }
-                    keyboard_hook::send_key(key_event);
-                }
-                KeyAction::VirtualKey(virtual_key) => {
-                    log_line = format!("{} remapped to virtual key {:#04X}", log_line, virtual_key);
-                    key_event.key = KeyType::VirtualKey(virtual_key);
-                    keyboard_hook::send_key(key_event);
-                }
-            }
-            true
-        } else {
-            false
+        let Some(remap) = remap else {
+            return false;
         };
 
-        println!("{}", log_line);
-        handled
+        match remap {
+            KeyAction::Ignore => {
+                println!("{} ignored", key_event);
+            }
+            KeyAction::Character(c) => {
+                if let Some(virtual_key) = keyboard_hook::get_virtual_key(c) {
+                    println!("{} remapped to `{}` as virtual key", key_event, c);
+                    key_event.key = KeyType::VirtualKey(virtual_key);
+                } else {
+                    println!("{} remapped to `{}` as unicode input", key_event, c);
+                    key_event.key = KeyType::Unicode(c);
+                }
+                keyboard_hook::send_key(key_event);
+            }
+            KeyAction::VirtualKey(virtual_key) => {
+                println!("{} remapped to virtual key {:#04X}", key_event, virtual_key);
+                key_event.key = KeyType::VirtualKey(virtual_key);
+                keyboard_hook::send_key(key_event);
+            }
+        }
+        true
     });
 
     ui.set_hook(kbhook);
