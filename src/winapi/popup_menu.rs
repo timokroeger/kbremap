a93f36a6205497b05a5009781228b95dab8d3f4d
Timo Kröger <timokroeger93@gmail.com>
2024-06-09 08:35:00 +0200

    Revert "Improve PopupMenu API"
    This reverts commit 5e8147dbd79156556331443c5ea25fbe09f5edd6.

--- src/winapi/popup_menu.rs
+++ src/winapi/popup_menu.rs
@@ -1,66 +1,47 @@
 use std::ffi::CStr;
-use std::num::NonZeroI32;
 use std::ptr;
 
 use windows_sys::Win32::Foundation::*;
 use windows_sys::Win32::UI::WindowsAndMessaging::*;
 
-pub struct PopupMenu {
-    hmenu: HMENU,
-    id: i32,
-}
+pub struct PopupMenu(HMENU);
 
 impl PopupMenu {
     pub fn new() -> Self {
         let hmenu = unsafe { CreatePopupMenu() };
-        debug_assert_ne!(hmenu, 0);
-        Self { hmenu, id: 0 }
+        assert_ne!(hmenu, 0);
+        Self(hmenu)
     }
 
-    pub fn add_entry(&mut self, text: &CStr, checked: bool, disabled: bool) -> MenuEntry {
-        self.id += 1;
-        let mut flags = 0;
-        if checked {
-            flags |= MF_CHECKED;
-        }
-        if disabled {
-            flags |= MF_DISABLED;
-        }
-        let result = unsafe {
-            AppendMenuA(
-                self.hmenu,
-                flags,
-                self.id as u32 as usize,
-                text.as_ptr().cast(),
-            )
-        };
-        debug_assert_ne!(result, 0);
-        MenuEntry(NonZeroI32::new(self.id).unwrap())
+    pub fn add_entry(&self, id: u32, flags: u32, text: &CStr) {
+        assert_ne!(id, 0, "menu entry id cannot be zero");
+        let result = unsafe { AppendMenuA(self.0, flags, id as usize, text.as_ptr().cast()) };
+        assert_ne!(result, 0);
     }
 
-    pub fn show(&self, hwnd: HWND, pt: POINT) -> Option<MenuEntry> {
+    pub fn show(&self, hwnd: HWND, pt: POINT) -> Option<u32> {
         unsafe {
             // Required for the menu to disappear when it loses focus.
             SetForegroundWindow(hwnd);
             let id = TrackPopupMenuEx(
-                self.hmenu,
+                self.0,
                 TPM_BOTTOMALIGN | TPM_NONOTIFY | TPM_RETURNCMD,
                 pt.x,
                 pt.y,
                 hwnd,
                 ptr::null(),
             );
-            let id = NonZeroI32::new(id)?;
-            Some(MenuEntry(id))
+            if id == 0 {
+                None
+            } else {
+                Some(id as u32)
+            }
         }
     }
 }
 
 impl Drop for PopupMenu {
     fn drop(&mut self) {
-        unsafe { DestroyMenu(self.hmenu) };
+        unsafe { DestroyMenu(self.0) };
     }
 }
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct MenuEntry(NonZeroI32);
