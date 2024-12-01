timokroeger/kbremap

commit cd1ebecf6deee31b666948a9de294c5085df5a8a

Do not include config path in instance id

src/main.rs
@@ -5,9 +5,7 @@ mod resources;
 mod winapi;
 
 use std::cell::RefCell;
-use std::collections::hash_map::DefaultHasher;
 use std::ffi::{CString, OsStr};
-use std::hash::{Hash, Hasher};
 use std::path::{Path, PathBuf};
 use std::{env, fs};
 
@@ -38,7 +36,19 @@ fn config_path(config_file: &OsStr) -> Result<PathBuf> {
     ))
 }
 
-fn register_keyboard_hook(config: &'static Config) -> KeyboardHook<impl FnMut(KeyEvent) -> bool + 'static> {
+fn load_config() -> Result<Config> {
+    let config_file = env::args_os()
+        .nth(1)
+        .unwrap_or_else(|| "config.toml".into());
+    let config_file = config_path(&config_file)?;
+    let config_str = fs::read_to_string(config_file)?;
+    let config: ReadableConfig = toml::from_str(&config_str)?;
+    Ok(Config::try_from(config)?)
+}
+
+fn register_keyboard_hook(
+    config: &'static Config,
+) -> KeyboardHook<impl FnMut(KeyEvent) -> bool + 'static> {
     let mut kb = VirtualKeyboard::new(&config.layout);
 
     KeyboardHook::set(move |mut key_event| {
@@ -103,23 +113,14 @@ fn main() -> Result<()> {
     // Not only checks if we are running from a terminal but also attaches to it.
     let running_in_terminal = winapi::console_check();
 
-    let config_file = env::args_os()
-        .nth(1)
-        .unwrap_or_else(|| "config.toml".into());
-    let config_file = config_path(&config_file)?;
-
     // Prevent duplicate instances if windows re-runs autostarts when rebooting after OS updates.
-    let mut hasher = DefaultHasher::new();
-    env::current_exe()?.hash(&mut hasher);
-    config_file.hash(&mut hasher);
-    let instance_key = CString::new(format!("kbremap-{:016x}", hasher.finish())).unwrap();
+    let current_exe = env::current_exe()?;
+    let instance_key = CString::new(current_exe.to_string_lossy().as_bytes())?;
     if !winapi::register_instance(&instance_key) {
         return Err(anyhow!("already running with the same configuration"));
     }
 
-    let config_str = fs::read_to_string(config_file)?;
-    let config: ReadableConfig = toml::from_str(&config_str)?;
-    let config = Config::try_from(config)?;
+    let config = load_config()?;
     let config = Box::leak(Box::new(config));
 
     let kbhook = &RefCell::new(Some(register_keyboard_hook(config)));

src/winapi/util.rs
@@ -11,16 +11,14 @@ use windows_sys::Win32::UI::WindowsAndMessaging::*;
 pub fn register_instance(name: &CStr) -> bool {
     unsafe {
         let handle = CreateMutexA(ptr::null(), 0, name.as_ptr().cast());
-        assert_ne!(handle, 0);
-
-        if GetLastError() != ERROR_ALREADY_EXISTS {
+        if handle == 0 && GetLastError() == ERROR_ALREADY_EXISTS {
+            CloseHandle(handle);
+            false
+        } else {
             // Intentionally leak the mutex object to protect this instance
             // of the process.
-            return true;
+            true
         }
-
-        CloseHandle(handle);
-        false
     }
 }
 

commit 86280fdb627ef3352901605a57574b5e9152b9f8

Remove broken single instance code
Regressed in cd1ebec. I have not noticed any problem with multiple instances
since then, meaning this feature is not required.

src/main.rs
@@ -6,11 +6,11 @@ mod util;
 mod winapi;
 
 use std::cell::Cell;
-use std::ffi::{CString, OsStr};
+use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 use std::{env, fs};
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result};
 use kbremap::{Config, KeyAction, ReadableConfig, VirtualKeyboard};
 use windows_sys::Win32::UI::Input::KeyboardAndMouse::VK_CAPITAL;
 use windows_sys::Win32::UI::WindowsAndMessaging::*;
@@ -97,13 +97,6 @@ impl App {
 }
 
 fn main() -> Result<()> {
-    // Prevent duplicate instances if windows re-runs autostarts when rebooting after OS updates.
-    let current_exe = env::current_exe()?;
-    let instance_key = CString::new(current_exe.to_string_lossy().as_bytes())?;
-    if !winapi::register_instance(&instance_key) {
-        return Err(anyhow!("another instance is already running"));
-    }
-
     let app: &App = Box::leak(Box::new(App::new()));
 
     let config = load_config()?;

src/winapi/util.rs
@@ -1,26 +1,9 @@
-use std::ffi::CStr;
 use std::ptr;
 
 use windows_sys::Win32::Foundation::*;
 use windows_sys::Win32::Storage::FileSystem::*;
 use windows_sys::Win32::System::Console::*;
-use windows_sys::Win32::System::Threading::CreateMutexA;
 use windows_sys::Win32::UI::WindowsAndMessaging::*;
-
-// Returns true when this process is the first instance with the given name.
-pub fn register_instance(name: &CStr) -> bool {
-    unsafe {
-        let handle = CreateMutexA(ptr::null(), 0, name.as_ptr().cast());
-        if handle.is_null() && GetLastError() == ERROR_ALREADY_EXISTS {
-            CloseHandle(handle);
-            false
-        } else {
-            // Intentionally leak the mutex object to protect this instance
-            // of the process.
-            true
-        }
-    }
-}
 
 // Attaches to the terminal when running from command line.
 // Returns true when a terminal to print stdout is available.