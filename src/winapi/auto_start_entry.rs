c77d0a9c880a1ff8e18408098813fac4fced1013
Timo Kröger <timokroeger93@gmail.com>
2025-12-30 10:23:04 +0100

    Refactor auto start entry code
    Better isolate unsafe winapi calls.
    Extract executable path query to function which now uses
    `QueryFullProcessImageNameA` instead of `GetModuleFileNameA` to get stable
    path for executable which does not depend on how it was launched.

--- src/winapi/auto_start_entry.rs
+++ src/winapi/auto_start_entry.rs
@@ -2,7 +2,10 @@ use std::{ffi::CStr, mem, ptr};
 
 use windows_sys::Win32::{
     Foundation::MAX_PATH,
-    System::{LibraryLoader::GetModuleFileNameA, Registry::*},
+    System::{
+        Registry::*,
+        Threading::{GetCurrentProcess, QueryFullProcessImageNameA},
+    },
 };
 
 pub struct AutoStartEntry<'a> {
@@ -34,62 +37,47 @@ impl<'a> AutoStartEntry<'a> {
     }
 
     pub fn is_registered(&self) -> bool {
-        unsafe {
-            let mut path_buf_reg = [0_u8; MAX_PATH as usize];
-            let mut path_len_reg = mem::size_of_val(&path_buf_reg) as u32;
-            let key_exists = RegGetValueA(
+        let mut path_buf_reg = [0_u8; MAX_PATH as usize];
+        let mut path_len_reg = mem::size_of_val(&path_buf_reg) as u32;
+        let key_exists = unsafe {
+            RegGetValueA(
                 self.key,
                 ptr::null(),
                 self.name.as_ptr().cast(),
                 RRF_RT_REG_SZ,
                 ptr::null_mut(),
-                ptr::addr_of_mut!(path_buf_reg).cast(),
+                path_buf_reg.as_mut_ptr().cast(),
                 &mut path_len_reg,
-            ) == 0;
-            if !key_exists {
-                return false;
-            }
-
-            let mut path_buf_exe = [0_u8; MAX_PATH as usize];
-            let mut path_len_exe = GetModuleFileNameA(
-                ptr::null_mut(),
-                path_buf_exe.as_mut_ptr(),
-                path_buf_exe.len() as _,
-            ) as usize;
-            if path_len_exe == 0 || path_len_exe == path_buf_exe.len() {
-                return false;
-            }
-
-            // Add 1 to the length of the executable path to include the null
-            // terminator in the string comparison.
-            path_len_exe += 1;
-            path_buf_exe[..path_len_exe] == path_buf_reg[..path_len_reg as usize]
+            )
+        } == 0;
+        if !key_exists {
+            return false;
         }
+
+        let mut path_buf_exe = [0_u8; MAX_PATH as usize];
+        let path_buf_exe = exe_path(&mut path_buf_exe);
+        if path_buf_exe.is_empty() {
+            return false;
+        }
+
+        path_buf_exe == &path_buf_reg[..path_len_reg as usize]
     }
 
     pub fn register(&self) {
+        let mut path_buf_exe = [0_u8; MAX_PATH as usize];
+        let path_buf_exe = exe_path(&mut path_buf_exe);
+        if path_buf_exe.is_empty() {
+            return;
+        }
+
         unsafe {
-            let mut path_buf_exe = [0_u8; MAX_PATH as usize];
-            let mut path_len_exe = GetModuleFileNameA(
-                ptr::null_mut(),
-                path_buf_exe.as_mut_ptr(),
-                path_buf_exe.len() as u32,
-            ) as usize;
-            if path_len_exe == 0 || path_len_exe == path_buf_exe.len() {
-                return;
-            }
-
-            // Add 1 to the length of the executable path to include the null
-            // terminator as required by the `RegSetValueEx()` function.
-            path_len_exe += 1;
-
             RegSetValueExA(
                 self.key,
                 self.name.as_ptr().cast(),
                 0,
                 REG_SZ,
-                path_buf_exe[..path_len_exe].as_ptr().cast(),
-                path_len_exe as _,
+                path_buf_exe.as_ptr().cast(),
+                path_buf_exe.len() as _,
             )
         };
     }
@@ -99,4 +87,21 @@ impl<'a> AutoStartEntry<'a> {
             RegDeleteValueA(self.key, self.name.as_ptr().cast());
         }
     }
+}
+
+// Returns the path to the current executable as a byte slice including the null
+// terminator. Returns a zero length slice on failure.
+fn exe_path(path_buf: &mut [u8]) -> &[u8] {
+    let mut buf_len = path_buf.len() as u32;
+    let ok = unsafe {
+        QueryFullProcessImageNameA(GetCurrentProcess(), 0, path_buf.as_mut_ptr(), &mut buf_len)
+    };
+    if ok == 0 {
+        return &[];
+    }
+    let len = buf_len as usize + 1; // Include null terminator
+    if len > path_buf.len() {
+        return &[];
+    }
+    &path_buf[..len]
 }
