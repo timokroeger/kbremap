565aa04e32f20982e2a649c8f441bdef48ccdd98
Timo Kröger <timokroeger93@gmail.com>
2026-01-11 16:08:17 +0100

    rename file

--- src/winapi/mod.rs
+++ src/winapi/mod.rs
@@ -1,10 +1,10 @@
 mod auto_start_entry;
+mod console;
 pub mod keyboard;
 mod static_icon;
 mod tray_icon;
-mod util;
 
 pub use auto_start_entry::*;
+pub use console::*;
 pub use static_icon::*;
 pub use tray_icon::*;
-pub use util::*;
