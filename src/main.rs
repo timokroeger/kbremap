timokroeger/kbremap

commit 6dd68dfa84cbe1b0a18c04c395faa802c0911a98

New strategy for low-level keyboard hook remapping
Do not run the remapping logic from within the low-level keyboard hook.
This caused a lot of problems with re-entrancy when sending inputs.
If a key is unmapped we do no longer forward it in the hook, instead we
enqueue all captured key events for later processing. The actual
processing now happens from an async task. We must ensure to re-send
also the unmapped keys from the processing taks.
The new approach has the downside that we catch every key event and
downstream keyboards hooks only ever see injected events. So far this
did not cause any issues in practice.

src/main.rs
@@ -11,12 +11,11 @@ use std::{env, fs, process};
 
 use anyhow::{Context, Result};
 use kbremap::{Config, KeyAction, ReadableConfig, VirtualKeyboard};
-use winapi::TrayIconEvent;
 use windows_sys::Win32::UI::Input::KeyboardAndMouse::VK_CAPITAL;
 use windows_sys::Win32::UI::WindowsAndMessaging::*;
-use winmsg_executor::{FilterResult, MessageLoop};
 
-use crate::winapi::{AutoStartEntry, KeyEvent, KeyType, StaticIcon, TrayIcon};
+use crate::winapi::keyboard::{self, KeyEvent, KeyType};
+use crate::winapi::{AutoStartEntry, StaticIcon, TrayIcon, TrayIconEvent};
 
 fn config_path(config_file: &OsStr) -> Result<PathBuf> {
     let mut path_buf;
@@ -56,6 +55,7 @@ struct App {
 
 impl App {
     fn new() -> Self {
+        keyboard::hook_enable();
         Self {
             // Display debug and panic output when launched from a terminal.
             // Not only checks if we are running from a terminal but also attaches to it.
@@ -87,25 +87,20 @@ impl App {
             self.tray_icon
                 .set_icon(StaticIcon::from_rc_numeric(resources::ICON_KEYBOARD_DELETE));
             self.enabled.set(false);
+            keyboard::hook_disable();
         } else {
             self.tray_icon
                 .set_icon(StaticIcon::from_rc_numeric(resources::ICON_KEYBOARD));
             self.enabled.set(true);
+            keyboard::hook_enable();
         }
     }
 }
 
-fn main() -> Result<()> {
-    let app: &App = Box::leak(Box::new(App::new()));
-
-    let config = load_config()?;
+async fn remap_keys(config: Config) {
     let mut kb = VirtualKeyboard::new(config.layout);
-    winapi::register_keyboard_hook(move |mut key_event| {
-        if !app.enabled.get() {
-            kb.reset();
-            println!("{} forwarded because remapping is disabled", key_event);
-            return false;
-        }
+    loop {
+        let mut key_event = keyboard::next_key_event().await;
 
         let remap = if key_event.up {
             kb.release_key(key_event.scan_code)
@@ -116,13 +111,13 @@ fn main() -> Result<()> {
         // Special caps lock handling:
         // Make sure the caps lock state stays in sync with the configured layer.
         if let Some(caps_lock_layer) = &config.caps_lock_layer {
-            if (kb.locked_layer() == caps_lock_layer) != winapi::caps_lock_enabled() {
-                winapi::send_key(KeyEvent {
+            if (kb.locked_layer() == caps_lock_layer) != keyboard::caps_lock_enabled() {
+                keyboard::send_key(KeyEvent {
                     up: false,
                     key: KeyType::VirtualKey(VK_CAPITAL as _),
                     ..key_event
                 });
-                winapi::send_key(KeyEvent {
+                keyboard::send_key(KeyEvent {
                     up: true,
                     key: KeyType::VirtualKey(VK_CAPITAL as _),
                     ..key_event
@@ -131,42 +126,33 @@ fn main() -> Result<()> {
             }
         }
 
-        let Some(remap) = remap else {
-            println!("{} forwarded", key_event);
-            return false;
-        };
-
-        let mapped_key = match remap {
-            KeyAction::Ignore => {
+        match remap {
+            None => println!("{} forwarded", key_event),
+            Some(KeyAction::Ignore) => {
                 println!("{} ignored", key_event);
-                return true;
+                continue;
             }
-            KeyAction::Character(c) => {
-                if let Some(virtual_key) = winapi::get_virtual_key(c) {
-                    print!("{} remapped to `{}` as virtual key", key_event, c);
-                    KeyType::VirtualKey(virtual_key)
+            Some(KeyAction::Character(c)) => {
+                if let Some(virtual_key) = keyboard::get_virtual_key(c) {
+                    println!("{} remapped to `{}` as virtual key", key_event, c);
+                    key_event.key = KeyType::VirtualKey(virtual_key);
                 } else {
-                    print!("{} remapped to `{}` as unicode input", key_event, c);
-                    KeyType::Unicode(c)
+                    println!("{} remapped to `{}` as unicode input", key_event, c);
+                    key_event.key = KeyType::Unicode(c);
                 }
             }
-            KeyAction::VirtualKey(virtual_key) => {
-                print!("{} remapped to virtual key {:#04X}", key_event, virtual_key);
-                KeyType::VirtualKey(virtual_key)
+            Some(KeyAction::VirtualKey(virtual_key)) => {
+                println!("{} remapped to virtual key {:#04X}", key_event, virtual_key);
+                key_event.key = KeyType::VirtualKey(virtual_key);
             }
         };
 
-        if matches!((mapped_key, key_event.key), (KeyType::VirtualKey(vk), KeyType::VirtualKey(prev_vk)) if vk == prev_vk)
-        {
-            println!(", forwarded");
-            return false;
-        }
+        keyboard::send_key(key_event);
+    }
+}
 
-        println!(", enqueued");
-        key_event.key = mapped_key;
-        winapi::send_key(key_event);
-        true
-    });
+fn main() -> Result<()> {
+    let app = Box::leak(Box::new(App::new()));
 
     const MENU_STARTUP: u32 = 1;
     const MENU_DEBUG: u32 = 2;
@@ -203,8 +189,8 @@ fn main() -> Result<()> {
         }
     });
 
-    // Event loop required for the low-level keyboard hook and the tray icon.
-    MessageLoop::run(|_this, _msg| FilterResult::Forward);
+    // The executor runs the windows message loop internally.
+    winmsg_executor::block_on(remap_keys(load_config()?));
 
     Ok(())
 }

src/winapi/keyboard.rs
@@ -13,44 +13,50 @@ use windows_sys::Win32::UI::Input::KeyboardAndMouse::*;
 use windows_sys::Win32::UI::WindowsAndMessaging::*;
 
 thread_local! {
-    /// Stores a type-erased pointer to the hook closure.
-    static HOOK: Cell<*mut ()> = const { Cell::new(ptr::null_mut()) };
     /// Buffer key events to prevent blocking the low-level keyboard hook.
     static KEY_QUEUE: RefCell<KeyQueue> = const { RefCell::new(KeyQueue::new()) };
+    static HOOK_HANDLE: Cell<HHOOK> = const { Cell::new(ptr::null_mut()) };
 }
 
-/// Sets the low-level keyboard hook for this thread.
-///
-/// The closure receives key press and key release events. When the closure
-/// returns `false`, the key event is not modified and forwarded as if
-/// nothing happened. To ignore a key event or to remap it to another
-/// key, return `true` and use [`send_key()`].
-///
-/// A message loop must be running on the same thread for the hook to work.
-///
-/// # Panics
-///
-/// Panics when a hook is already registered from the same thread.
-pub fn register_keyboard_hook<F>(callback: F)
-where
-    F: FnMut(KeyEvent) -> bool + 'static,
-{
-    assert!(
-        HOOK.get().is_null(),
-        "Only one keyboard hook can be registered per thread."
-    );
+/// Installs the low-level keyboard hook for this thread.
+/// Warning: Captures all keyboard events system-wide, no other application will
+/// receive keyboard events until the hook is removed by calling `hook_disable()`.
+pub fn hook_enable() {
+    if HOOK_HANDLE.get().is_null() {
+        let handle =
+            unsafe { SetWindowsHookExA(WH_KEYBOARD_LL, Some(hook_proc), ptr::null_mut(), 0) };
+        assert!(
+            !handle.is_null(),
+            "Failed to install low-level keyboard hook."
+        );
+        HOOK_HANDLE.set(handle);
+    }
+}
 
-    winmsg_executor::spawn_local(send_key_task());
+/// Removes the low-level keyboard hook for this thread.
+pub fn hook_disable() {
+    let handle = HOOK_HANDLE.get();
+    if !handle.is_null() {
+        unsafe {
+            UnhookWindowsHookEx(handle);
+        }
+        HOOK_HANDLE.set(ptr::null_mut());
+    }
+}
 
-    let callback = Box::into_raw(Box::new(callback));
-    HOOK.set(callback as *mut ());
-
-    let handle =
-        unsafe { SetWindowsHookExA(WH_KEYBOARD_LL, Some(hook_proc::<F>), ptr::null_mut(), 0) };
-    assert!(
-        !handle.is_null(),
-        "Failed to install low-level keyboard hook."
-    );
+/// Asynchronously waits for the next key event captured by the low-level keyboard hook.
+pub async fn next_key_event() -> KeyEvent {
+    poll_fn(|cx| {
+        KEY_QUEUE.with_borrow_mut(|queue| {
+            if let Some(key) = queue.key_events.pop_front() {
+                Poll::Ready(key)
+            } else {
+                queue.waker.push(cx.waker().clone());
+                Poll::Pending
+            }
+        })
+    })
+    .await
 }
 
 /// Key event type.
@@ -119,10 +125,7 @@ impl KeyEvent {
 /// The actual WinAPI compatible hook callback function.
 /// Called from `KiUserCallbackDispatcher()` context as described in
 /// (this blog post)[http://www.nynaeve.net/?p=204]. Might be re-entered.
-unsafe extern "system" fn hook_proc<F>(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT
-where
-    F: FnMut(KeyEvent) -> bool + 'static,
-{
+unsafe extern "system" fn hook_proc(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
     if code != HC_ACTION as i32 {
         return CallNextHookEx(ptr::null_mut(), code, wparam, lparam);
     }
@@ -139,86 +142,44 @@ where
 
     // Windows re-enters the hook function for two conditions:
     // 1. `SendInput()` called from within the hook, which produces an injected
-    //    message. We pass on injected messages without looking at them anyway.
+    //    key. Additionally to the injected key, all other regular key events
+    //    are passed to the hook before `SendInput()` returns.
     // 2. The hook blocks longer than the number of ms specified in the registry
     //    key `HKEY_CURRENT_USER\Control Panel\LowLevelHooksTimeout`.
+    // Our solution to both scenarios is to buffer all key events and process
+    // them asynchronously outside of the hook context.
+    // This has the downside that we catch every key event and downstream
+    // keyboards hooks only ever see injected events. So far this did not cause
+    // any issues in practice.
 
-    // As the main use case for a low-level keyboard hook is to remap keys,
-    // we implement a non-blocking `send_key()` function (see below) which can
-    // safely be called from the hook.
-
-    // The TLS `HOOK` variable contains a null pointer while the user-provided
-    // closure is running. We can use that to detect if the hook was re-entered.
-    // Only call the closure when we are sure it is available.
-    let hook_ptr = HOOK.replace(ptr::null_mut()) as *mut F;
-    if let Some(hook) = unsafe { hook_ptr.as_mut() } {
-        let handled = hook(KeyEvent::from_hook_lparam(hook_lparam));
-        HOOK.set(hook_ptr as *mut ());
-        if handled {
-            return -1;
-        }
-    }
-
-    CallNextHookEx(ptr::null_mut(), code, wparam, lparam)
+    let key = KeyEvent::from_hook_lparam(hook_lparam);
+    KEY_QUEUE.with(|queue| queue.borrow_mut().enqueue(key));
+    -1
 }
 
 struct KeyQueue {
     key_events: VecDeque<KeyEvent>,
-    waker: Option<Waker>,
+    waker: Vec<Waker>,
 }
 
 impl KeyQueue {
     const fn new() -> Self {
         Self {
             key_events: VecDeque::new(),
-            waker: None,
+            waker: Vec::new(),
         }
     }
 
     fn enqueue(&mut self, key: KeyEvent) {
         self.key_events.push_back(key);
-        if let Some(waker) = self.waker.take() {
+        for waker in self.waker.drain(..) {
             waker.wake();
         }
     }
 }
 
 /// Sends a virtual key event.
-// TODO: Send keys directly when not called from within the keyboard hook.
 pub fn send_key(key: KeyEvent) {
-    // `SendInput()` may block if a slow/faulty low-level keyboard hook is
-    // registered. As a fix, buffer the key event and call `SendInput()` from
-    // an async task running on our threads message loop (which only can run
-    // after we have returned from the hook procedure).
-    // That way our hook is unaffected by other faulty hooks and Windows can
-    // correctly remove the offending hooks from the hook chain.
-    KEY_QUEUE.with(|queue| queue.borrow_mut().enqueue(key));
-}
-
-async fn send_key_task() {
-    poll_fn(|cx| {
-        while let Some(key) = KEY_QUEUE.with_borrow_mut(|queue| queue.key_events.pop_front()) {
-            // The internally used `SendInput()` function may re-enter the
-            // keyboard hook. It does so by calling it with an injected version
-            // of the key event we sent. But also for other pending real key
-            // events. To be able to enqueue more key events from within the
-            // hook (e.g. as reaction to a real key press), we must not hold a
-            // mutable borrow on the key queue when calling `send_input()`.
-                send_input(key);
-            }
-
-        KEY_QUEUE.with_borrow_mut(|queue| {
-            // We are the only task waiting for key events.
-            debug_assert!(queue.waker.is_none());
-            queue.waker = Some(cx.waker().clone());
-        });
-            Poll::Pending::<()>
-        })
-    .await;
-}
-
-// Delayed handler called by the async task to actually send the key event.
-fn send_input(key: KeyEvent) {
     unsafe {
         let mut inputs: [INPUT; 2] = mem::zeroed();
 

src/winapi/mod.rs
@@ -1,11 +1,10 @@
 mod auto_start_entry;
-mod keyboard;
+pub mod keyboard;
 mod static_icon;
 mod tray_icon;
 mod util;
 
 pub use auto_start_entry::*;
-pub use keyboard::*;
 pub use static_icon::*;
 pub use tray_icon::*;
 pub use util::*;

commit 7824ff60bbeaf09f4e14185dff0aedb917999c9a

Reset virtual keyboard state on re-enable
This regressed in 6dd68dfa84cbe1b0a18c04c395faa802c0911a98

src/main.rs
@@ -45,11 +45,18 @@ fn load_config() -> Result<Layout> {
     Layout::parse_toml(&fs::read_to_string(config_file)?)
 }
 
+#[derive(Debug, Clone, Copy)]
+enum EnableState {
+    Enabled,
+    Disabled,
+    ReEnabled,
+}
+
 struct App {
     running_in_terminal: bool,
     autostart: AutoStartEntry<'static>,
     tray_icon: TrayIcon,
-    enabled: Cell<bool>,
+    enable_state: Cell<EnableState>,
 }
 
 impl App {
@@ -61,7 +68,7 @@ impl App {
             running_in_terminal: winapi::console_check(),
             autostart: AutoStartEntry::new(c"kbremap"),
             tray_icon: TrayIcon::new(StaticIcon::from_rc_numeric(resources::ICON_KEYBOARD)),
-            enabled: Cell::new(true),
+            enable_state: Cell::new(EnableState::Enabled),
         }
     }
 
@@ -82,24 +89,32 @@ impl App {
     }
 
     fn toggle_enabled(&self) {
-        if self.enabled.get() {
-            self.tray_icon
-                .set_icon(StaticIcon::from_rc_numeric(resources::ICON_KEYBOARD_DELETE));
-            self.enabled.set(false);
-            keyboard::hook_disable();
-        } else {
-            self.tray_icon
-                .set_icon(StaticIcon::from_rc_numeric(resources::ICON_KEYBOARD));
-            self.enabled.set(true);
-            keyboard::hook_enable();
+        match self.enable_state.get() {
+            EnableState::Enabled | EnableState::ReEnabled => {
+                self.tray_icon
+                    .set_icon(StaticIcon::from_rc_numeric(resources::ICON_KEYBOARD_DELETE));
+                self.enable_state.set(EnableState::Disabled);
+                keyboard::hook_disable();
+            }
+            EnableState::Disabled => {
+                self.tray_icon
+                    .set_icon(StaticIcon::from_rc_numeric(resources::ICON_KEYBOARD));
+                self.enable_state.set(EnableState::ReEnabled);
+                keyboard::hook_enable();
+            }
         }
     }
 }
 
-async fn remap_keys(layout: Layout) {
+async fn remap_keys(layout: Layout, enable_state: &Cell<EnableState>) {
     let mut kb = VirtualKeyboard::new(layout);
     loop {
         let mut key_event = keyboard::next_key_event().await;
+
+        if matches!(enable_state.get(), EnableState::ReEnabled) {
+            kb.reset();
+            enable_state.set(EnableState::Enabled);
+        }
 
         let remap = if key_event.up {
             kb.release_key(key_event.scan_code)
@@ -151,6 +166,7 @@ async fn remap_keys(layout: Layout) {
 }
 
 fn main() -> Result<()> {
+    let layout = load_config()?;
     let app = Box::leak(Box::new(App::new()));
 
     const MENU_STARTUP: u32 = 1;
@@ -172,7 +188,11 @@ fn main() -> Result<()> {
             flag_checked(winapi::console_check()) | flag_disabled(app.running_in_terminal),
             c"Show debug output",
         );
-        menu.add_entry(MENU_DISABLE, flag_checked(!app.enabled.get()), c"Disable");
+        menu.add_entry(
+            MENU_DISABLE,
+            flag_checked(matches!(app.enable_state.get(), EnableState::Disabled)),
+            c"Disable",
+        );
         menu.add_entry(MENU_EXIT, 0, c"Exit");
     });
 
@@ -189,7 +209,7 @@ fn main() -> Result<()> {
     });
 
     // The executor runs the windows message loop internally.
-    winmsg_executor::block_on(remap_keys(load_config()?));
+    winmsg_executor::block_on(remap_keys(layout, &app.enable_state));
 
     Ok(())
 }